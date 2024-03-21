dbbbcfc023395fa72ff6a202363c0928348fa458
Kauê Rodrigues <kaue.rodrigueskrb@gmail.com>
2024-03-21 13:32:12 -0300

    Implementação bem sucedida do Stepfun

--- Experiencia03/VHDL/Stepfun.vhd
+++ Experiencia03/VHDL/Stepfun.vhd
@@ -1,13 +1,16 @@
-entity somador is 
-port(
-	x, y : in bit_vector(31 downto 0);
-    q : out bit_vector(31 downto 0)
-);
-end somador;
+library IEEE;
+use IEEE.numeric_bit.all;
 
+entity somador is 
+    port(
+        x, y : in bit_vector(31 downto 0);
+        q : out bit_vector(31 downto 0)
+    );
+end somador;
+    
 architecture arch_somador of somador is
 begin 
-	
+    
     q <= bit_vector(unsigned(x) + unsigned(y));
     
 end arch_somador;
@@ -36,56 +39,56 @@ end arch_somador;
 
 entity stepfun is 
 port(
-	ai, bi, ci, di, ei, fi, gi, hi : in bit_vector(31 downto 0);
+    ai, bi, ci, di, ei, fi, gi, hi : in bit_vector(31 downto 0);
     kpw : in bit_vector(31 downto 0);
     ao, bo, co, do, eo, fo, go, ho : out bit_vector(31 downto 0)
 );
 end stepfun;
 
 architecture arch_stepfun of stepfun is
-	
+    
     -- Components 
     component somador is 
-	port(
-		x, y : in bit_vector(31 downto 0);
-    	q : out bit_vector(31 downto 0)
-	);
-	end component;
+    port(
+        x, y : in bit_vector(31 downto 0);
+        q : out bit_vector(31 downto 0)
+    );
+    end component;
     
     component ch is 
-	port(
-		x, y, z : in bit_vector(31 downto 0);
-    	q : out bit_vector(31 downto 0)
-	);
-	end component;
+    port(
+        x, y, z : in bit_vector(31 downto 0);
+        q : out bit_vector(31 downto 0)
+    );
+    end component;
     
     component sum0 is 
-	port(
-		x : in bit_vector(31 downto 0);
-    	q : out bit_vector(31 downto 0)
-	);
-	end component;
+    port(
+        x : in bit_vector(31 downto 0);
+        q : out bit_vector(31 downto 0)
+    );
+    end component;
     
     component sum1 is 
-	port(
-		x : in bit_vector(31 downto 0);
-    	q : out bit_vector(31 downto 0)
-	);
-	end component;
+    port(
+        x : in bit_vector(31 downto 0);
+        q : out bit_vector(31 downto 0)
+    );
+    end component;
     
     component maj is 
-	port(
-		x, y, z : in bit_vector(31 downto 0);
-    	q : out bit_vector(31 downto 0)
-	);
-	end component;
+    port(
+        x, y, z : in bit_vector(31 downto 0);
+        q : out bit_vector(31 downto 0)
+    );
+    end component;
     
     --component extende_palavra is 
-	--port(
-	--	x : in bit_vector(7 downto 0);
+    --port(
+    --	x : in bit_vector(7 downto 0);
     --	q : out bit_vector(31 downto 0)
-	--);
-	--end component;
+    --);
+    --end component;
     
     -- Signals 
     signal soma1, soma2, soma3, soma4, soma5, soma6 : bit_vector(31 downto 0);
@@ -93,25 +96,25 @@ architecture arch_stepfun of stepfun is
     --entrada_serial : bit_vector(7 downto 0); -- Aqui eh a ideia. No codigo da entrega vem da pinagem
 
 begin 
-	
+    
     -- Replicação da palavra para formar as entradas
     -- ext1: extende_palavra port map(entrada_serial, ai);
     -- ext2: extende_palavra port map(entrada_serial, bi);
     -- etc ...
     
     -- Somas
-    sum1: somador port map(hi, kpw, soma1);
-    sum2: somador port map(ch_out, soma1, soma2);
-    sum3: somador port map(sum1_out, soma2, soma3);
-    sum4: somador port map(maj_out, soma3, soma4);
-    sum5: somador port map(sum0_out, soma4, soma5);
-    sum6: somador port map(di, soma3, soma6);
+    soma1_port: somador port map(hi, kpw, soma1);
+    soma2_port: somador port map(ch_out, soma1, soma2);
+    soma3_port: somador port map(sum1_out, soma2, soma3);
+    soma4_port: somador port map(maj_out, soma3, soma4);
+    soma5_port: somador port map(sum0_out, soma4, soma5);
+    soma6_port: somador port map(di, soma3, soma6);
     
     -- Funções 
-    ch: ch port map(ei, fi, gi, ch_out);
-    sum0: sum0 port map(ai, sum0_out);
-    sum1: sum1 port map(ei, sum1_out);
-    maj: maj port map(ai, bi, ci, maj_out);
+    ch_port: ch port map(ei, fi, gi, ch_out);
+    sum0_port: sum0 port map(ai, sum0_out);
+    sum1_port: sum1 port map(ei, sum1_out);
+    maj_port: maj port map(ai, bi, ci, maj_out);
     
     -- Resultados
     ao <= soma5;
@@ -124,4 +127,3 @@ begin
     ho <= gi;
      
 end arch_stepfun;
-
