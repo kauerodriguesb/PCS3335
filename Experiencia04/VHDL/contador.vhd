2dfaaba79c674427f0038684c2df4d78840b5544
Kauê Rodrigues <kaue.rodrigueskrb@gmail.com>
2024-03-24 21:06:47 -0300

    Contador testado e funcionando

--- Experiencia04/VHDL/contador.vhd
+++ Experiencia04/VHDL/contador.vhd
@@ -11,32 +11,25 @@ use IEEE.numeric_bit.all;
 
 entity contador is
     Port (rst, clk  : in bit;
-          controle  : out bit_vector(5 downto 0); --Usado pra verificar na simulação 
           count_end : out bit
     );
 end contador;
 
 architecture arch_contador of contador is
-    signal internal : bit_vector(5 downto 0); -- Controle
 begin
     process(rst,clk)
         variable count : integer;
     begin
         if (rst = '0') then 
             count := 0;
-            internal <= "000000";
         elsif (clk'event and clk = '1') then 
             count := count + 1;
-            internal <= bit_vector(to_unsigned(count, internal'length));
         end if;
 
         if (count = 64) then
             count := 0;
             count_end <= '1';
-            internal <= "000000";
         end if;
     end process;
-
-    controle <= internal;
 
 end arch_contador;
